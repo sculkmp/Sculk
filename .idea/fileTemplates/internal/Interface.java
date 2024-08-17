d2305738b4cdda2e39439844c8122eb0ae047adc
JblusItsMe <jeanbaptiste.fourgeux@gmail.com>
2024-08-18 01:36:50 +0200

    add .idea

---
+++ .idea/fileTemplates/internal/Interface.java
@@ -0,0 +1,20 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};
+
+#end
+/*
+ *   ____             _ _              __  __ ____
+ *  / ___|  ___ _   _| | | __         |  \/  |  _ \
+ *  \___ \ / __| | | | | |/ /  _____  | |\/| | |_) |
+ *   ___) | (__| |_| | |   <  |_____| | |  | |  __/
+ *  |____/ \___|\__,_|_|_|\_\         |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author: SculkTeams
+ * @link: http://www.sculkmp.org/
+ */
+public interface ${NAME} {
+}
