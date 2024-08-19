9726e47716d736a44062f84be7a034a916f56c8a
JblusItsMe <jeanbaptiste.fourgeux@gmail.com>
2024-08-20 00:46:11 +0200

    get player skin for ClientChainData

---
+++ src/main/java/org/sculk/player/skin/data/ImageData.java
@@ -0,0 +1,30 @@
+package org.sculk.player.skin.data;
+
+
+import lombok.Data;
+
+/*
+ *   ____             _ _              __  __ ____
+ *  / ___|  ___ _   _| | | __         |  \/  |  _ \
+ *  \___ \ / __| | | | | |/ /  _____  | |\/| | |_) |
+ *   ___) | (__| |_| | |   <  |_____| | |  | |  __/
+ *  |____/ \___|\__,_|_|_|\_\         |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author: SculkTeams
+ * @link: http://www.sculkmp.org/
+ */
+@Data
+public class ImageData {
+
+    private final int width;
+    private final int height;
+    private final byte[] image;
+
+    public static final ImageData EMPTY = new ImageData(0, 0, new byte[0]);
+
+}
---
+++ src/main/java/org/sculk/player/skin/data/PersonaPiece.java
@@ -0,0 +1,30 @@
+package org.sculk.player.skin.data;
+
+
+import lombok.Data;
+
+/*
+ *   ____             _ _              __  __ ____
+ *  / ___|  ___ _   _| | | __         |  \/  |  _ \
+ *  \___ \ / __| | | | | |/ /  _____  | |\/| | |_) |
+ *   ___) | (__| |_| | |   <  |_____| | |  | |  __/
+ *  |____/ \___|\__,_|_|_|\_\         |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author: SculkTeams
+ * @link: http://www.sculkmp.org/
+ */
+@Data
+public class PersonaPiece {
+
+    private final String id;
+    private final String type;
+    private final String packId;
+    private final boolean isDefault;
+    private final String productId;
+
+}
