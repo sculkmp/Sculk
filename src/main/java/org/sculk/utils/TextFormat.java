2d974d4232f7e5b07f2401521a22ec22cf13c5e2
AzaleeX <azaleemcpe@gmail.com>
2024-08-20 23:14:07 +0200

    feat(colors): new minecraft colors added

--- src/main/java/org/sculk/utils/TextFormat.java
+++ src/main/java/org/sculk/utils/TextFormat.java
@@ -17,21 +17,39 @@ package org.sculk.utils;
  */
 public class TextFormat {
 
-    public static String BLACK =            "§0";
-    public static String DARK_BLUE =        "§1";
-    public static String DARK_GREEN =       "§2";
-    public static String DARK_AQUA =        "§3";
-    public static String DARK_RED =         "§4";
-    public static String DARK_PURPLE =      "§5";
-    public static String GOLD =             "§6";
-    public static String GRAY =             "§7";
-    public static String DARK_GRAY =        "§8";
-    public static String BLUE =             "§9";
-    public static String GREEN =            "§a";
-    public static String AQUA =             "§b";
-    public static String RED =              "§c";
-    public static String LIGHT_PURPLE =     "§d";
-    public static String YELLOW =           "§e";
-    public static String WHITE =            "§f";
+    public static String BLACK =                "§0";
+    public static String DARK_BLUE =            "§1";
+    public static String DARK_GREEN =           "§2";
+    public static String DARK_AQUA =            "§3";
+    public static String DARK_RED =             "§4";
+    public static String DARK_PURPLE =          "§5";
+    public static String GOLD =                 "§6";
+    public static String GRAY =                 "§7";
+    public static String DARK_GRAY =            "§8";
+    public static String BLUE =                 "§9";
+    public static String GREEN =                "§a";
+    public static String AQUA =                 "§b";
+    public static String RED =                  "§c";
+    public static String LIGHT_PURPLE =         "§d";
+    public static String YELLOW =               "§e";
+    public static String WHITE =                "§f";
+    public static String MINECOIN_GOLD =        "§g";
+    public static String MATERIAL_QUARTZ =      "§h";
+    public static String MATERIAL_IRON =        "§i";
+    public static String MATERIAL_NETHERITE =   "§j";
+    public static String MATERIAL_REDSTONE =    "§m";
+    public static String MATERIAL_COPPER =      "§n";
+    public static String MATERIAL_GOLD =        "§p";
+    public static String MATERIAL_EMERALD =     "§q";
+    public static String MATERIAL_DIAMOND =     "§s";
+    public static String MATERIAL_LAPIS =       "§t";
+    public static String MATERIAL_AMETHYST =    "§u";
 
+    // Style modifiers (italic, bold, underline, etc)
+    public static String OBFUSCATED =           "§k";
+    public static String BOLD =                 "§l";
+    public static String STRIKETHROUGH =        "§m";
+    public static String UNDERLINE =            "§n";
+    public static String ITALIC =               "§o";
+    public static String RESET =                "§r";
 }
