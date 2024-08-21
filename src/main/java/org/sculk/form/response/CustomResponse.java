f37d7b20ca304db830250a8a30d00be85432c88a
PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>
2024-08-21 16:34:53 +0200

    Add Getters for CustomResponse

--- src/main/java/org/sculk/form/response/CustomResponse.java
+++ src/main/java/org/sculk/form/response/CustomResponse.java
@@ -23,7 +23,7 @@ public class CustomResponse implements Response {
     protected final Int2BooleanOpenHashMap toggleResponses = new Int2BooleanOpenHashMap();
 
     public void setDropdownResponse(int index, ElementResponse response) {
-        this.responses.put(index, response.getElementText());
+        this.responses.put(index, response);
         this.dropdownResponses.put(index, response);
     }
 
@@ -43,12 +43,40 @@ public class CustomResponse implements Response {
     }
 
     public void setStepSliderResponse(int index, ElementResponse response) {
-        this.responses.put(index, response.getElementText());
+        this.responses.put(index, response);
         this.stepSliderResponses.put(index, response);
     }
 
     public void setToggleResponse(int index, boolean response) {
         this.responses.put(index, (Boolean) response);
         this.toggleResponses.put(index, response);
+    }
+
+    public Object getResponse(int index) {
+        return this.responses.get(index);
+    }
+
+    public ElementResponse getDropdownResponse(int index) {
+        return this.dropdownResponses.get(index);
+    }
+
+    public String getInputResponse(int index) {
+        return this.inputResponses.get(index);
+    }
+
+    public String getLabelResponse(int index) {
+        return this.labelResponses.get(index);
+    }
+
+    public float getSliderResponse(int index) {
+        return this.sliderResponses.get(index);
+    }
+
+    public ElementResponse getStepSliderResponse(int index) {
+        return this.stepSliderResponses.get(index);
+    }
+
+    public boolean getToggleResponse(int index) {
+        return this.toggleResponses.get(index);
     }
 }
