d6b0650dc2ed0b90d9fb4ddce20b44ffa416ba41
AzaleeX <azaleemcpe@gmail.com>
2024-08-21 16:38:46 +0200

    feat(server-properties): Creation of ServerProperties System (#18)
    * feat(server-properties): creation of a ServerProperties system with enums
    
    * feat(server-properties): delete "on" and "off"
    
    * feat(server-properties): add "SPAWN_ANIMALS" and "SPAWN_MONSTERS"

---
+++ src/main/java/org/sculk/config/ServerPropertiesKeys.java
@@ -0,0 +1,32 @@
+package org.sculk.config;
+
+public enum ServerPropertiesKeys {
+    LANGUAGE("language"),
+    MOTD("motd"),
+    SUB_MOTD("sub-motd"),
+    SERVER_PORT("server-port"),
+    SERVER_IP("server-ip"),
+    WHITELIST("white-list"),
+    MAX_PLAYERS("max-players"),
+    GAMEMODE("gamemode"),
+    PVP("pvp"),
+    DIFFICULTY("difficulty"),
+    LEVEL_NAME("level-name"),
+    LEVEL_SEED("level-seed"),
+    LEVEL_TYPE("level-type"),
+    SPAWN_ANIMALS("spawn-animals"),
+    SPAWN_MONSTERS("spawn-monsters"),
+    AUTO_SAVE("auto-save"),
+    XBOX_AUTH("xbox-auth");
+
+    private final String key;
+
+    ServerPropertiesKeys(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return key;
+    }
+}
