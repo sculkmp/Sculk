cedf50fb1892d4778437a533c074d88f4653d8bc
JblusItsMe <jeanbaptiste.fourgeux@gmail.com>
2024-08-24 06:42:47 +0200

    Adding a page system and information on a command with/help [page|command name]

--- src/main/java/org/sculk/command/Command.java
+++ src/main/java/org/sculk/command/Command.java
@@ -33,6 +33,7 @@ public abstract class Command {
     private String label;
     @Getter
     private String description;
+    @Getter
     private String usageMessage;
 
     @Getter
--- src/main/java/org/sculk/command/defaults/HelpCommand.java
+++ src/main/java/org/sculk/command/defaults/HelpCommand.java
@@ -1,9 +1,11 @@
 package org.sculk.command.defaults;
 
 import com.google.gson.Gson;
+import org.cloudburstmc.protocol.bedrock.data.command.CommandParamType;
 import org.sculk.Server;
 import org.sculk.command.Command;
 import org.sculk.command.CommandSender;
+import org.sculk.command.data.CommandParameter;
 import org.sculk.exception.CommandException;
 import org.sculk.permission.DefaultPermissionNames;
 import org.sculk.player.text.RawTextBuilder;
@@ -12,6 +14,7 @@ import org.sculk.player.text.TranslaterBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /*
  *   ____             _ _
@@ -33,30 +36,82 @@ public class HelpCommand extends Command {
     public HelpCommand() {
         super("help", "Show the help menu", "/help [page|command name]", List.of("?"));
         this.setPermission(DefaultPermissionNames.COMMAND_HELP);
+        this.registerParameter(new CommandParameter("page", CommandParamType.INT, true));
+        this.registerParameter(new CommandParameter("command", CommandParamType.TEXT, true));
     }
 
     @Override
     public void execute(CommandSender sender, String commandLabel, List<String> args) throws CommandException {
         StringBuilder builder = new StringBuilder();
-        TranslaterBuilder translaterBuilder = new TranslaterBuilder();
         List<String> commandSending = new ArrayList<>();
-        translaterBuilder.setTranslate("§6-------------- §fHelp - %%s command(s) §6--------------\n%%s");
         Server.getInstance().getCommandMap().getCommands().forEach((s, command) -> {
-        String commandName = s.contains(":") ? s.substring(s.indexOf(':') + 1) : s;
+            String commandName = s.contains(":") ? s.substring(s.indexOf(':') + 1) : s;
             if(!commandSending.contains(commandName)) {
-                builder.append("§6/").append(commandName).append(":§f ").append(command.getDescription()).append("\n");
                 commandSending.add(commandName);
             }
         });
 
+        int totalCommands = commandSending.size();
+        int commandsPerPage = 5;
+        int actualPage = 1;
+        int totalPage = (int) Math.ceil((double) totalCommands / commandsPerPage);
+
+        int startIndex = 0;
+        int endIndex = Math.min(startIndex + commandsPerPage, totalCommands);
+
+        if (!args.isEmpty()) {
+            try {
+                actualPage = Integer.parseInt(args.getFirst());
+                if (actualPage < 1 || actualPage > totalPage) {
+                    actualPage = 1;
+                }
+            } catch (NumberFormatException e) {
+                actualPage = 1;
+                String commandName = args.getFirst();
+                Command command = Server.getInstance().getCommandMap().getCommand(commandName);
+                try {
+                    sender.sendMessage(command.getLabel());
+                    sender.sendMessage(new RawTextBuilder().add(new TranslaterBuilder()
+                            .setTranslate("§6/%%s: §f%%s\nUsage: §e%%s")
+                            .setWith(new RawTextBuilder()
+                                    .add(new TextBuilder().setText(command.getLabel()))
+                                    .add(new TextBuilder().setText(command.getDescription()))
+                                    .add(new TextBuilder().setText(command.getUsageMessage()))
+                            )
+                    ));
+                } catch(RuntimeException exception) {
+                    sender.sendMessage(new RawTextBuilder().add(new TranslaterBuilder().setTranslate("§4/%%s§c does not seem to exist, checked the list of commands with §4/help§c.").setWith(new RawTextBuilder()
+                            .add(new TextBuilder().setText(args.getFirst()))
+                    )));
+                }
+                return;
+            }
+
+            startIndex = (actualPage - 1) * commandsPerPage;
+            endIndex = Math.min(startIndex + commandsPerPage, totalCommands);
+        }
+
+        for (int i = startIndex; i < endIndex; i++) {
+            String commandName = commandSending.get(i);
+            Command command = Server.getInstance().getCommandMap().getCommand(commandName);
+            if (command != null) {
+                builder.append("§6/").append(commandName).append(":§f ").append(command.getDescription()).append("\n");
+            }
+        }
+
+        TranslaterBuilder translaterBuilder = new TranslaterBuilder();
+        translaterBuilder.setTranslate("§6-------------- §fHelp - %%s command(s) §7[%%s/%%s] §6--------------\n%%s");
         translaterBuilder.setWith(new RawTextBuilder()
                 .add(new TextBuilder()
-                        .setText(Integer.toString(commandSending.size())))
+                        .setText(Integer.toString(totalCommands)))
                 .add(new TextBuilder()
-                        .setText(builder.substring(0, builder.length() - 2)))
+                        .setText(Integer.toString(actualPage)))
+                .add(new TextBuilder()
+                        .setText(Integer.toString(totalPage)))
+                .add(new TextBuilder()
+                        .setText(builder.substring(0, builder.length() - 1)))
         );
         sender.sendMessage(new RawTextBuilder().add(translaterBuilder));
-
     }
 
 }
