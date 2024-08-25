228957f25497206e96dc1329aa59c0cac13b7a19
Nyrok <47859726+Nyrok@users.noreply.github.com>
2024-08-25 09:44:20 +0200

    TerminalConsole: Add @Getter annotation

--- src/main/java/org/sculk/console/TerminalConsole.java
+++ src/main/java/org/sculk/console/TerminalConsole.java
@@ -26,9 +26,9 @@ import java.util.List;
  * @author: SculkTeams
  * @link: http://www.sculkmp.org/
  */
+@Getter
 public class TerminalConsole extends SimpleTerminalConsole implements CommandSender {
 
-    @Getter
     private final Server server;
     private final ConsoleThread consoleThread;
 
@@ -85,10 +85,6 @@ public class TerminalConsole extends SimpleTerminalConsole implements CommandSen
     @Override
     protected void shutdown() {
         Server.getInstance().shutdown();
-    }
-
-    public ConsoleThread getConsoleThread() {
-        return this.consoleThread;
     }
 
     @Override
