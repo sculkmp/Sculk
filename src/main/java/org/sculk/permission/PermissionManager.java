7884b44221e46e4fe9658d1cc43941a6f3f4a8f9
Nyrok <47859726+Nyrok@users.noreply.github.com>
2024-08-25 09:28:28 +0200

    Create PermissionManager.java

---
+++ src/main/java/org/sculk/permission/PermissionManager.java
@@ -0,0 +1,22 @@
+package org.sculk.permission;
+
+/*
+ *   ____             _ _
+ *  / ___|  ___ _   _| | | __
+ *  \___ \ / __| | | | | |/ /
+ *   ___) | (__| |_| | |   <
+ *  |____/ \___|\__,_|_|_|\_\
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author: SculkTeams
+ * @link: http://www.sculkmp.org/
+ */
+public class PermissionManager {
+    // TODO: Add Console to DefaultPermissionNames.GROUP_CONSOLE
+    // TODO: Add Player to DefaultPermissionNames.GROUP_USER
+    // TODO: Parse an ops.txt File to add OPs users to DefaultPermissionNames.GROUP_OPERATOR
+}
